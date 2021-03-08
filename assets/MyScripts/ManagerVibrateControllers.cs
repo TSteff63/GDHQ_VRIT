cefbfa65649510b6f31fa1dcbb1f416bc7021d66
TSteff63 <tsteff63@yahoo.com>
2021-03-08 13:43:13 -0500

    Controller vibration added into warp sequences

---
+++ assets/MyScripts/ManagerVibrateControllers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerVibrateControllers : MonoBehaviour
+{
+    public void VibrateControllers(float time)
+    {
+        // starts vibration on the right Touch controller
+        OVRInput.SetControllerVibration(0.5f, time, OVRInput.Controller.RTouch);
+
+        // starts vibration on the right Touch controller
+        OVRInput.SetControllerVibration(0.5f, time, OVRInput.Controller.LTouch);
+    }
+}
